db19b8c9f864cd34d3c5adec482443405a794a33
Adinã Matos <adinanmatosjunior021@gmail.com>
2026-08-24 17:01:29 -0400

    inicia a estrutura da product details page

--- pizzashop/src/pages/store/ProductDetailPage.tsx
+++ pizzashop/src/pages/store/ProductDetailPage.tsx
@@ -2,7 +2,7 @@ export function ProductDetailPage() {
   return (
     <div>
       <h1>Detalhes do Produto</h1>
-      <p>Aqui vai ficar a foto e o preço da pizza.</p>
+      <p>Aqui vai ficar a foto e o preço da pizza oi.</p>
     </div>
   );
 }
